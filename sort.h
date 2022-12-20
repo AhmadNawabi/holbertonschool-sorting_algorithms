0cca770e2ffc02140352b32ba3ca57391cd5a4b4
AhmadNawabi <ahmadshamoon0505@gmail.com>
2022-12-20 08:11:56 -0800

    hello

--- sort.h
+++ sort.h
@@ -1,50 +1,33 @@
-#ifndef SORT_H
-#define SORT_H
-
-#include <print_array.c>
-#include <print_list.c>
-#include <stdlib.h>
-#include <stdio.h>
-#include "sort.h"
-
+##ifndef H
+#define H
+#undef H
+/*
+ * Includes
+ */
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
- * @n: Integer  stored in the node
+ * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
+ * Description: struct
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+        const int n;
+        struct listint_s *prev;
+        struct listint_s *next;
 } listint_t;
-
-/* Prototypes for mandatory tasks */
+/*
+ * Prototypes
+ */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-size_t partition(int array[], size_t size);
-void print_sort(int array[], size_t size, int init);
-
-/* Prototypes for advanced tasks */
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-
-/* Prototypes for print functions */
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
-
-/* Prototypes for helper functions */
-void swap(int *a, int *b);
-void heapify(int *array, int end, int start, size_t size);
-
-#endif /* SORT_H */
+void quick_sort(int *array, size_t size);
+void partition(int *array, int low, int high, size_t size);
+void array_swap(int *array, int a, int b);
+#endif
